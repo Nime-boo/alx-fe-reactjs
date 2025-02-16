7a716f0cec8dd7f4867c2487dd03518156b646c4
Nims <ebongofonime98@gmail.com>
2025-02-16 23:53:15 +0100

    Added JSX updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -9,6 +12,11 @@ function App() {
 
   return (
     <>
+    <div>
+        <Header />
+        <MainContent />
+        <Footer />
+    </div>
     <div>
     <WelcomeMessage />
     </div>
