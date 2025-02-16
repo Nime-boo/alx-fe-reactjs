511393a16206295e778115b0ae3eed883204bd30
Nims <ebongofonime98@gmail.com>
2025-02-17 00:04:54 +0100

    Added JSX updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
@@ -19,6 +20,7 @@ function App() {
     </div>
     <div>
     <WelcomeMessage />
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
       <div>
         <a href="https://vite.dev" target="_blank">
