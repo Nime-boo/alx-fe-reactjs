f2ae996aa643536e6afce594bf8c4a9d0226b377
Nims <ebongofonime98@gmail.com>
2025-02-23 23:38:40 +0100

    Applied inline styling to components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MainContent = () => {
   return (
     <main style={{ padding: "20px", backgroundColor: "#f4f4f4", borderRadius: "10px" }}>
-      <p style={{ fontSize: "1.2rem", textAlign: "justify" }}>
+      <p style={{ fontSize: "1.2rem", textAlign: "justify", color: "grey" }}>
         Welcome to my favorite cities page. Here, you will find details about some of the most beautiful places in the world!
       </p>
     </main>
