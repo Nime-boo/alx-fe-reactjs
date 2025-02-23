680e0c876ada9d8e0ac754e78b715a2f9ea1346a
Nims <ebongofonime98@gmail.com>
2025-02-23 23:28:21 +0100

    Initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: "20px", backgroundColor: "#f4f4f4", borderRadius: "10px" }}>
+      <p style={{ fontSize: "1.2rem", textAlign: "justify" }}>
+        Welcome to my favorite cities page. Here, you will find details about some of the most beautiful places in the world!
+      </p>
+    </main>
+  );
+};
+
+export default MainContent;
