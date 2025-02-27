1388d6ea09457ad95df837e763e7b9dc85f70e6c
Nims <ebongofonime98@gmail.com>
2025-02-27 07:20:34 +0100

    Updated User.Context.js

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import UserContext from './context/UserContext';
+import UserContext from './components/UserContext';
 import ProfilePage from './components/ProfilePage';
 import UserProfile from './components/UserProfile';
 import Header from './components/Header';
